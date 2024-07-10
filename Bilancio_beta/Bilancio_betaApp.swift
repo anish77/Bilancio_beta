c12e844d1f244cec76f2dcbba1edc2bec10fd607
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-07-10 22:32:28 +0200

    Initial Commit

---
+++ Bilancio_beta/Bilancio_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bilancio_betaApp.swift
+//  Bilancio_beta
+//
+//  Created by Ana Cvasniuc on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Bilancio_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bilancio_beta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bilancio_beta
+//
+//  Created by Ana Cvasniuc on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
